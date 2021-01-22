127ba4caf4ac5b4328f1f897b521c92d48070780
zishang520 <zishang520@gmail.com>
2021-01-22 16:13:59 +0800

    更新群组类型

--- src/Type.php
+++ src/Type.php
@@ -3,9 +3,25 @@ namespace luoyy\Tim;
 
 class Type
 {
-    const PRIVATE_ROOM = 'Private';
+    const WORK_ROOM = 'Work';
     const PUBLIC_ROOM = 'Public';
-    const CHAT_ROOM = 'ChatRoom';
+    const MEETING_ROOM = 'Meeting';
     const AV_CHAT_ROOM = 'AVChatRoom';
+
+    /**
+     * 使用 Work 替代
+     * @deprecated
+     */
+    const PRIVATE_ROOM = 'Private';
+    /**
+     * 使用 Meeting 替代
+     * @deprecated
+     */
+    const CHAT_ROOM = 'ChatRoom';
+
+     /**
+     * 无替代方案，可以考虑 AVChatRoom
+     * @deprecated
+     */
     const B_CHAT_ROOM = 'BChatRoom';
 }
